e3e74af34e2f6899ecc8640203ad08ff37baa869
Dmitriy-Ilin <Ilin.Dima-1605982014@yandex.ru>
2025-02-22 22:34:58 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { VacanciesProvider } from './context/vacancy-context.tsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <VacanciesProvider>
         <App />
       </VacanciesProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
 
